080bdbfa4aed20213bb0e988ddf5a2c1f0d4eb0e
Rohit-S-Singh <rohitshekrsingh@gmail.com>
2023-03-12 12:24:53 +0530

    project setup and legacy code

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Fall-Detection-using-YOLO-and-Machine-Learning
+
+<!-- Project is Under Devlopment -->
+
+Fall Detection using YOLO and Machine Learning
+This project is a fall detection system that uses the You Only Look Once (YOLO) object detection algorithm and machine learning techniques to detect whether a person has fallen or not.
+
+Requirements
+The following libraries are required to run this project:
+
+OpenCV
+NumPy
+scikit-learn
+Keras
+TensorFlow
+You can install these libraries using pip:
+
+bash
+Copy code
+pip install opencv-python numpy scikit-learn keras tensorflow
+Dataset
+The dataset used to train the machine learning model consists of videos of people falling and performing daily activities. The videos were collected from various sources, such as YouTube, and manually annotated to label each frame as either a fall or a non-fall event.
+
+YOLO Object Detection
+The YOLO algorithm is used to detect people in each frame of the video. The YOLOv3 implementation from the Darknet project is used, which can be found at https://github.com/pjreddie/darknet.
+
+Machine Learning
+After detecting people in each frame of the video, features are extracted from each person using a pre-trained convolutional neural network (CNN). The features are then fed into a support vector machine (SVM) classifier to determine if the person has fallen or not.
+
+Usage
+To use this fall detection system, run the following command:
+
+bash
+Copy code
+python detect_fall.py --video <path-to-video>
+This will detect falls in the specified video and output the results to the console.
+
+References
+Redmon, Joseph, and Ali Farhadi. "YOLOv3: An incremental improvement." arXiv preprint arXiv:1804.02767 (2018).
+Chollet, François. "Deep learning with Python." Manning Publications Co. (2018).
+https://github.com/pjreddie/darknet
---
+++ static/js/main.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  // Init
+  $(".image-section").hide();
+  $(".loader").hide();
+  $("#result").hide();
+  $("#error").hide();
+  $("#success").hide();
+
+  // Upload Preview
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $("#imagePreview").css(
+          "background-image",
+          "url(" + e.target.result + ")"
+        );
+        $("#imagePreview").hide();
+        $("#imagePreview").fadeIn(650);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#imageUpload").change(function () {
+    $(".image-section").show();
+    $("#btn-predict").show();
+    $("#result").text("");
+    $("#result").hide();
+    readURL(this);
+  });
+
+  // Predict
+  $("#btn-predict").click(function () {
+    var form_data = new FormData($("#upload-file")[0]);
+
+    // Show loading animation
+    $(this).hide();
+    $(".loader").show();
+
+    // Make prediction by calling api /predict
+    $.ajax({
+      type: "POST",
+      url: "/predict",
+      data: form_data,
+      contentType: false,
+      cache: false,
+      processData: false,
+      async: true,
+      success: function (data) {
+        // Get and display the result
+        $(".loader").hide();
+        $("#result").fadeIn(600);
+        if (data !== "Safe Driving") {
+          $("#success").hide();
+          $("#error").show();
+          $("#error").html(
+            "<h3>State: Distracted</h3><br><h4>Activity Detected:  " +
+              data +
+              "</h4>"
+          );
+        } else {
+          $("#error").hide();
+          $("#success").show();
+          $("#success").html(
+            "<h3>State: Attentive</h3><br><h4>Activity Detected:  " +
+              data +
+              "</h4>"
+          );
+        }
+        // $("#result").text("" + data);
+        console.log("Success!");
+      },
+    });
+  });
+});
